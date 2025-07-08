fe3473ee76ca3101d60e3a0e9e37d9db90593a88
imjustdad <cjohnson41@outlook.com>
2025-07-07 19:23:41 -0500

    initial

---
+++ bot/app.py
@@ -0,0 +1,317 @@
+from multiprocessing import Process
+import os
+import re
+import logging
+import datetime
+import json
+import requests
+import praw
+import pymongo
+from rich.pretty import pprint
+
+# region Environment Variables
+REDDIT_CLIENT_ID = os.getenv('REDDIT_CLIENT_ID')
+REDDIT_CLIENT_SECRET = os.getenv('REDDIT_CLIENT_SECRET')
+REDDIT_USER_AGENT = os.getenv('REDDIT_USER_AGENT')
+
+MONGO_URI = os.getenv('MONGO_URI')
+
+DISCORD_WEBHOOK_URL = os.getenv('DISCORD_WEBHOOK_URL')
+# endregion
+
+# region Logging
+
+# region app Logging
+app_logger = logging.getLogger('app')
+app_logger.setLevel(logging.DEBUG)
+
+format_handler = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+file_handler = logging.FileHandler('app.log')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(format_handler)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO)
+stream_handler.setFormatter(format_handler)
+
+app_logger.addHandler(file_handler)
+app_logger.addHandler(stream_handler)
+# endregion
+
+# region PRAW Logging
+praw_file_handler = logging.FileHandler('praw.log')
+praw_file_handler.setLevel(logging.WARNING)  # DEBUG to enable these logs
+praw_file_handler.setFormatter(format_handler)
+
+for logger_name in ('praw', 'prawcore'):
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(praw_file_handler)
+
+# endregion
+
+# region pymongo Logging (Partially Working)
+pymongo_logger = logging.getLogger('pymongo.command')
+pymongo_logger.setLevel(logging.DEBUG)
+
+pymongo_file_handler = logging.FileHandler('pymongo.log')
+pymongo_file_handler.setLevel(logging.DEBUG)
+pymongo_file_handler.setFormatter(format_handler)
+
+# === uncomment the below 3 comments to write command logs to the terminal
+# pymongo_stream_handler = logging.StreamHandler()
+# pymongo_stream_handler.setLevel(logging.DEBUG)
+
+pymongo_logger.addHandler(pymongo_file_handler)
+# pymongo_logger.addHandler(pymongo_stream_handler)
+# endregion
+
+# endregion Logging
+
+# region PRAW Configuration
+praw_config = {
+    'client_id': REDDIT_CLIENT_ID,
+    'client_secret': REDDIT_CLIENT_SECRET,
+    'user_agent': REDDIT_USER_AGENT,
+    'ratelimit_seconds': 600
+}
+
+praw_subreddit = 'patfinnerty'
+# endregion
+
+# region MongoDB Configuration
+client = pymongo.MongoClient(MONGO_URI, server_api=pymongo.server_api.ServerApi(
+    version='1', strict=True, deprecation_errors=True))
+database = client['main']
+submissions_collection = database['submissions']
+comments_collection = database['comments']
+# endregion
+
+chord_progression_pattern = re.compile(
+    r'(\s|^)(I IV V|I V vi IV|ii V I|vi IV I V|I vi IV V|iii vi IV V|I IV vi V|I vi ii V|IV V I|I iii IV V|1 4 5|1 5 6 4|2 5 1|6 4 1 5|1 6 4 5|3 6 4 5|1 4 6 5|1 6 2 5|4 5 1|1 3 4 5)(\?+|!+|\.+|\,+)?(\s|$)')
+chord_names_pattern = re.compile(
+    r'(\s|^)(C|C#|D|D#|E|F|F#|G|G#|A|A#|B|Cb|Db|Eb|Fb|Gb|Ab|Bb)(m|7|m7|dim|aug|sus[24]|6|9|11|13|#9|b5|b9|#5|maj|maj7|maj9|7b5|m7b5)?(\?+|!+|\.+|\,+)?(\s|$)')
+
+
+def is_pattern_matched(text):
+    return chord_names_pattern.search(text) or chord_progression_pattern.search(text)
+
+
+def convert_to_datetime(utc_time):
+    return datetime.datetime.fromtimestamp(utc_time).strftime('%Y-%m-%d %H:%M:%S')
+
+
+def send_discord_webhook(entity, document):
+
+    if entity == 'comment':
+        data = {
+            'content': 'a new document was inserted into mongo',
+            'embeds': [
+                {
+                    'title': 'new comment',
+                    'fields': [
+                        {
+                            'name': '_id',
+                            'value': f"{document['_id']}",
+                            'inline': False
+                        },
+                        {
+                            'name': 'body',
+                            'value': f"{document['body']}",
+                            'inline': False
+                        },
+                        {
+                            'name': 'permalink',
+                            'value': f"{document['permalink']}",
+                            'inline': False
+                        }
+                    ],
+                    'color': 3066993
+                }
+            ]
+        }
+
+    elif entity == 'submission':
+        data = {
+            'content': 'a new document was inserted into mongo',
+            'embeds': [
+                {
+                    'title': 'new submission',
+                    'fields': [
+                        {
+                            'name': '_id',
+                            'value': f"{document['_id']}",
+                            'inline': False
+                        },
+                        {
+                            'name': 'title',
+                            'value': f"{document['title']}",
+                            'inline': False
+                        }
+                    ],
+                    'color': 3066993
+                }
+            ]
+        }
+
+    response = requests.post(
+        DISCORD_WEBHOOK_URL, data=json.dumps(data), headers={'Content-Type': 'application/json'})
+
+    if response.status_code == 204:
+        app_logger.info('f"{entity}" document sent to discord')
+
+
+def is_submission_persisted(submission_id):
+    document = submissions_collection.find_one({'_id': submission_id})
+
+    if document:
+        app_logger.info(
+            f"found submission collection document {document['_id']}")
+        app_logger.info(pprint(document))
+        return True
+    else:
+        app_logger.info(f'no submission collection document found')
+        return False
+
+
+def save_submission_to_db(submission):
+    data = {
+        '_id': submission.id,
+        'title': submission.title,
+        'author': {
+            'id': submission.author.id,
+            'name': submission.author.name,
+            'url': f'https://www.reddit.com/u/{submission.author.name}'
+        },
+        'url': submission.url,
+        'created': convert_to_datetime(submission.created_utc)
+    }
+
+    try:
+        document = submissions_collection.insert_one(data)
+        inserted_id = document.inserted_id
+        inserted_document = submissions_collection.find_one(
+            {'_id': inserted_id})
+
+        send_discord_webhook('submission', inserted_document)
+
+        app_logger.info(f'Submission {submission.id} saved to database')
+        app_logger.info(f'{pprint(inserted_document)}')
+
+    except Exception as e:
+        app_logger.error(f'Error saving submission: {e}')
+
+
+def is_comment_persisted(comment_id):
+    document = comments_collection.find_one({'_id': comment_id})
+
+    if document:
+        app_logger.info(
+            f"found comment collection document: {document['_id']}")
+        app_logger.info(pprint(document))
+        return True
+    else:
+        app_logger.info(f'no comment collection document found')
+        return False
+
+
+def save_comment_to_db(comment):
+    data = {
+        '_id': comment.id,
+        'body': comment.body,
+        'author': {
+            'id': comment.author.id,
+            'name': comment.author.name,
+            'url': f'https://www.reddit.com/u/{comment.author.name}'
+        },
+        'submission': {
+            'id': comment.submission.id,
+            'title': comment.submission.title,
+            'author': {
+                'id': comment.submission.author.id,
+                'name': comment.submission.author.name,
+                'url': f'https://www.reddit.com/u/{comment.submission.author.name}'
+            },
+            'url': comment.submission.url,
+            'created': convert_to_datetime(comment.submission.created_utc)
+        },
+        'permalink': f'https://www.reddit.com{comment.permalink}',
+        'created': convert_to_datetime(comment.created_utc)
+    }
+
+    try:
+        document = comments_collection.insert_one(data)
+        inserted_id = document.inserted_id
+        inserted_document = comments_collection.find_one({'_id': inserted_id})
+
+        send_discord_webhook('comment', inserted_document)
+
+        app_logger.info(f'Comment {comment.id} saved to database')
+        app_logger.info(f'{pprint(inserted_document)}')
+
+    except Exception as e:
+        app_logger.error(f'Error saving comment: {e}')
+
+
+def stream_submissions():
+    app_logger.info(
+        f'started watching for new submissions on r/{praw_subreddit}...')
+
+    reddit = praw.Reddit(**praw_config)
+
+    subreddit = reddit.subreddit(praw_subreddit)
+
+    for submission in subreddit.stream.submissions(skip_existing=True):
+
+        match = is_pattern_matched(submission.title)
+
+        if match:
+            app_logger.info(
+                f'submission pattern matched - pattern: "{match.group()}" id: {submission.id} link: {submission.url}\n{submission.title}')
+
+            if not is_submission_persisted(submission.id):
+                app_logger.info(
+                    f'saving submission to database: {submission.title}')
+
+                # submission.reply('Beato')
+
+                save_submission_to_db(submission)
+
+
+def stream_comments():
+    app_logger.info(
+        f'started watching for new comments on r/{praw_subreddit}...')
+    reddit = praw.Reddit(**praw_config)
+
+    subreddit = reddit.subreddit(praw_subreddit)
+
+    for comment in subreddit.stream.comments(skip_existing=True):
+
+        match = is_pattern_matched(comment.body)
+
+        if match:
+            app_logger.info(
+                f'comment pattern matched - pattern: "{match.group()}" id: {comment.id} link: {comment.permalink}\n{comment.body}')
+
+            if not is_comment_persisted(comment.id):
+                app_logger.info(f'saving comment to database: {comment.id}')
+
+                # comment.reply('Beato')
+
+                save_comment_to_db(comment)
+
+
+def main():
+    p1 = Process(target=stream_submissions)
+    p2 = Process(target=stream_comments)
+
+    p1.start()
+    p2.start()
+
+    p1.join()
+    p2.join()
+
+
+if __name__ == '__main__':
+    main()
---
+++ server/server.py
@@ -0,0 +1,77 @@
+# -----------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See LICENSE in the project root for license information.
+# -----------------------------------------------------------------------------------------
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from llama_cpp import Llama
+from typing import List, Union
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+app = FastAPI()
+
+llm = Llama.from_pretrained(
+    repo_id="hugging-quants/Llama-3.2-1B-Instruct-Q4_K_M-GGUF",
+    filename="llama-3.2-1b-instruct-q4_k_m.gguf"
+)
+
+
+class ChatRequest(BaseModel):
+    message: str
+
+
+class ChatResponse(BaseModel):
+    status: str
+    matches: Union[List[str], None]
+    message: str
+
+
+def analyze_response(message: str) -> List[str]:
+    try:
+        response = llm.create_chat_completion(
+            messages=[
+                {"role": "system", "content": "Please analyze the following message for musical chords or progressions. Progressions may also be listed as numbers rather than roman numerals. If you find any chords or progressions, just list them, separated by commas. If no chords or progressions are found, say 'no matches detected.' Do not provide any additional explanation."},
+                {
+                    "role": "user",
+                    "content": message
+                }
+            ]
+        )
+
+        response_text = response['choices'][0]['message']['content']
+
+        if "no matches detected" in response_text:
+            return []
+
+        matches = [match.strip()
+                   for match in response_text.split(',') if match.strip()]
+
+        return matches
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.post("/llama", response_model=ChatResponse)
+async def chat_completion(request: ChatRequest):
+    try:
+        matches = analyze_response(request.message)
+
+        if matches:
+            return {
+                "status": "success",
+                "matches": matches,
+                "message": "Matches found and returned"
+            }
+        else:
+            return {
+                "status": "success",
+                "matches": [],
+                "message": "No matches detected"
+            }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
