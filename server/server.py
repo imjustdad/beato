c1c4e6e1cf896cee020bc37e99780b3f73e30021
imjustdad <cjohnson41@outlook.com>
2025-07-11 00:35:34 -0500

    removed comments from devpod

--- server/server.py
+++ server/server.py
@@ -1,8 +1,3 @@
-# -----------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license information.
-# -----------------------------------------------------------------------------------------
-
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from llama_cpp import Llama
